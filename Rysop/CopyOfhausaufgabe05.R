ba4a28b18519d1ccb6d38666660ec9c09dc4a03c
Anna Rysop <rysop@students.uni-marburg.de>
2014-05-04 21:58:52 +0200

    Datum angepasst

--- Rysop/CopyOfhausaufgabe05.R
+++ Rysop/CopyOfhausaufgabe05.R
@@ -1,6 +1,6 @@
-# Hausaufgabe 04
+# Hausaufgabe 05
 # Anna Uta Rysop <rysop@students.uni-marburg.de>
-# 2014-05-02
+# 2014-05-04
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
